6ba7ea9ac4409dd357ce101319f8b63c50af2e09
Sona <Sona@Sona-studio>
2010-03-16 16:11:00 +0200

    urls fo ststic serve

--- urls.py
+++ urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
+
+admin.site.root_path = "/admin/" # there is probably a bug in django...
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^vsad/', include('vsad.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+    
+    # just for testing - native way to sampleapp urls 
+    # (r'^sampleapp-native/', include('sampleapp.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns+= patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
+    )
+
